55d5868686de82b983e954f883b3a6a399b66313
Ayrat <ayrat1@mail.ru>
2018-02-04 18:23:55 +0300

    Initial commit

---
+++ android/src/de/android/ayrathairullin/rover/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.android.ayrathairullin.rover;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.android.ayrathairullin.rover.Rover;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Rover(), config);
+	}
+}
---
+++ desktop/src/de/android/ayrathairullin/rover/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.android.ayrathairullin.rover.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.android.ayrathairullin.rover.Rover;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rover(), config);
+	}
+}
