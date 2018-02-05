db73ff8df7b9103924a1856635ddb62adf899e70
Ayrat <ayrat1@mail.ru>
2018-02-05 21:19:34 +0300

    complete v3 00.53

--- core/src/de/android/ayrathairullin/rover/Rover.java
+++ core/src/de/android/ayrathairullin/rover/Rover.java
@@ -1,12 +1,12 @@
 package de.android.ayrathairullin.rover;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Rover extends ApplicationAdapter {
+public class Rover extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
