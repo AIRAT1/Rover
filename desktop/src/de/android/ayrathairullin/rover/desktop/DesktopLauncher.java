575340eb5b944f420122fd61244f19d5ed455373
Ayrat <ayrat1@mail.ru>
2018-02-06 23:41:39 +0300

    complete v5 02.34

--- desktop/src/de/android/ayrathairullin/rover/desktop/DesktopLauncher.java
+++ desktop/src/de/android/ayrathairullin/rover/desktop/DesktopLauncher.java
@@ -2,11 +2,26 @@ package de.android.ayrathairullin.rover.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import de.android.ayrathairullin.rover.GameCallback;
 import de.android.ayrathairullin.rover.Rover;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Rover(), config);
+	public static void main(String[] args) {
+		new DesktopLauncher();
 	}
+
+	public DesktopLauncher() {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Rover(callback), config);
+	}
+
+	private GameCallback callback = new GameCallback() {
+		@Override
+		public void sendMessage(int message) {
+			System.out.println("DesktopLauncher sendMessage: " + message);
+		}
+	};
 }
