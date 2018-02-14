51364ad3faf73ab20f1c7111c7412a6f3235733e
Ayrat <ayrat1@mail.ru>
2018-02-14 17:52:58 +0300

    complete v10 level1 work

--- core/src/de/android/ayrathairullin/rover/Setting.java
+++ core/src/de/android/ayrathairullin/rover/Setting.java
@@ -7,6 +7,7 @@ public class Setting {
     public static final float GRAVITY = 8;
     public static final float JUMP_IMPULSE = 80;
     public static final float WHEEL_TORQUE = 50;
+
     public static final String ADMOB_BANNER = "";
     public static final String ADMOB_INTERSTITIAL = "";
     public static final String TEST_DEVICE = "";
