492994c4b018dd2c0d15d984300e52de6ed30836
Ayrat <ayrat1@mail.ru>
2018-02-15 22:15:55 +0300

    first release version

--- android/src/de/android/ayrathairullin/rover/AndroidLauncher.java
+++ android/src/de/android/ayrathairullin/rover/AndroidLauncher.java
@@ -2,6 +2,7 @@ package de.android.ayrathairullin.rover;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,20 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class AndroidLauncher extends AndroidApplication {
 	private RelativeLayout mainView;
 	private AdView bannerView;
 	private ViewGroup bannerContainer;
 	private RelativeLayout.LayoutParams bannerParams;
+	private InterstitialAd interstitial;
+	private boolean interstitialHasInited = false;
+	private boolean enableAdmobInterstitials;
 
 
 	@Override
@@ -37,6 +45,12 @@ public class AndroidLauncher extends AndroidApplication {
 		bannerContainer = new LinearLayout(this);
 		mainView.addView(bannerContainer, bannerParams);
 		bannerContainer.setVisibility(View.GONE);
+		if (Setting.ADMOB_INTERSTITIAL == null) {
+			enableAdmobInterstitials = false;
+		}else {
+			enableAdmobInterstitials = true;
+		}
+		new AdmobBannerTask().execute();
 	}
 
 	private GameCallback gameCallback = new GameCallback() {
@@ -46,30 +60,30 @@ public class AndroidLauncher extends AndroidApplication {
 				AndroidLauncher.this.runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						bannerContainer.setVisibility(View.VISIBLE);
+						showBanner();
 					}
 				});
 			}else if (message == Rover.HIDE_BANNER) {
 				AndroidLauncher.this.runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-
+						hideBanner();
 					}
 				});
 			}else if (message == Rover.LOAD_INTERSTITIAL) {
-
-			}else if (message == Rover.OPEN_MARKET) {
-				Uri uri = Uri.parse(getString(R.string.share_url));
-//				Uri uri = Uri.parse("market://details?id=de.android.ayrathairullin.rover");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+				loadAdmobInterstitial();
 			}else if (message == Rover.SHOW_INTERSTITIAL) {
 				AndroidLauncher.this.runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-
+						showAdmobInterstitial();
 					}
 				});
+			}else if (message == Rover.OPEN_MARKET) {
+				Uri uri = Uri.parse(getString(R.string.share_url));
+//				Uri uri = Uri.parse("market://details?id=de.android.ayrathairullin.rover");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(intent);
 			}else if (message == Rover.SHARE) {
                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
@@ -79,6 +93,104 @@ public class AndroidLauncher extends AndroidApplication {
                 sharingIntent.putExtra(Intent.EXTRA_TEXT, body);
                 startActivity(Intent.createChooser(sharingIntent, getString(R.string.share_via)));
 			}
+		}
+	};
+
+	private class AdmobBannerTask extends AsyncTask<Void, Void, AdView> {
+
+		@Override
+		protected AdView doInBackground(Void... voids) {
+			AdView adView = new AdView(AndroidLauncher.this);
+			return adView;
+		}
+
+		@Override
+		protected void onPostExecute(AdView adView) {
+			bannerView = adView;
+			bannerView.setAdSize(AdSize.BANNER);
+			bannerView.setAdUnitId(Setting.ADMOB_BANNER);
+			AdRequest adRequest = new AdRequest.Builder()
+					.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+					.addTestDevice(Setting.TEST_DEVICE)
+					.build();
+			bannerView.setAdListener(bannerListener);
+			bannerView.loadAd(adRequest);
+		}
+	}
+
+	private AdListener bannerListener = new AdListener() {
+		@Override
+		public void onAdFailedToLoad(int i) {
+			super.onAdFailedToLoad(i);
+		}
+
+		@Override
+		public void onAdLoaded() {
+			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+			lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			if (bannerView.getParent() == null) {
+				bannerContainer.addView(bannerView, lp);
+			}
+			super.onAdLoaded();
+		}
+	};
+
+	private void showBanner() {
+		bannerContainer.setVisibility(View.VISIBLE);
+	}
+
+	private void hideBanner() {
+		bannerContainer.setVisibility(View.GONE);
+	}
+
+	private void loadAdmobInterstitial() {
+		if (interstitialHasInited) return;
+		interstitialHasInited = true;
+		if (!enableAdmobInterstitials) return;
+		new AdmobInterstitialTask().execute();
+	}
+
+	private void showAdmobInterstitial() {
+		if (interstitial == null) return;
+		if (interstitial.isLoaded()) {
+			interstitial.show();
+			interstitialHasInited = false;
+		}
+	}
+
+	private class AdmobInterstitialTask extends AsyncTask<Void, Void, AdRequest> {
+
+		@Override
+		protected AdRequest doInBackground(Void... voids) {
+			AdRequest adRequest = new AdRequest.Builder()
+					.addTestDevice(Setting.TEST_DEVICE).build();
+			if (interstitial != null) {
+				return adRequest;
+			}
+			interstitial = new InterstitialAd(AndroidLauncher.this);
+			interstitial.setAdUnitId(Setting.ADMOB_INTERSTITIAL);
+			interstitial.setAdListener(interstitialListener);
+			return adRequest;
+		}
+
+		@Override
+		protected void onPostExecute(AdRequest adRequest) {
+			if (adRequest == null) return;
+			interstitial.loadAd(adRequest);
+		}
+	}
+
+	private AdListener interstitialListener = new AdListener() {
+		@Override
+		public void onAdFailedToLoad(int i) {
+			interstitialHasInited = false;
+		}
+
+		@Override
+		public void onAdLoaded() {
+
 		}
 	};
 }
--- core/src/de/android/ayrathairullin/rover/Setting.java
+++ core/src/de/android/ayrathairullin/rover/Setting.java
@@ -8,7 +8,7 @@ public class Setting {
     public static final float JUMP_IMPULSE = 80;
     public static final float WHEEL_TORQUE = 50;
 
-    public static final String ADMOB_BANNER = "";
-    public static final String ADMOB_INTERSTITIAL = "";
-    public static final String TEST_DEVICE = "";
+    public static final String ADMOB_BANNER = "ca-app-pub-6708703349449969/2341576553";
+    public static final String ADMOB_INTERSTITIAL = "ca-app-pub-6708703349449969/4082157346";
+    public static final String TEST_DEVICE = "30D9300605DD60CE60905D4B3C11B63F";
 }
