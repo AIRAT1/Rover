8b9d2445dc4aa28bdc44d7881d1f4f032d87a341
Ayrat <ayrat1@mail.ru>
2018-02-15 22:21:14 +0300

    first release version

--- core/src/de/android/ayrathairullin/rover/Setting.java
+++ core/src/de/android/ayrathairullin/rover/Setting.java
@@ -2,7 +2,7 @@ package de.android.ayrathairullin.rover;
 
 
 public class Setting {
-    public static final boolean DEBUG_GAME = true;
+    public static final boolean DEBUG_GAME = false;
     public static final boolean DEBUG_WORLD = false;
     public static final float GRAVITY = 8;
     public static final float JUMP_IMPULSE = 80;
